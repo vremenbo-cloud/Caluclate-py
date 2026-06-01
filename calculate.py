76d7205b6d3e764a4076ac4bd12033da710c75a4
Noname <vremenbo@gmail.com>
2026-06-01 20:57:27 +0000

    calculate

---
+++ calculate.py
@@ -0,0 +1,10 @@
+a, b = null, null
+print("Введите первое число: ")
+input(a)
+print("Введите второе число: ")
+input(b)
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
+# мне в падлу писать свитчи и ифы так что так 
